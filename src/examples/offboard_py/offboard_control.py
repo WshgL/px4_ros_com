d43e01d4d210001d5a0cbb992abdaf41e26de9cd
WshgL <wilsonleong@foxmail.com>
2023-06-07 16:41:21 +0200

    publisher & subscriber: 10 -> 5

--- src/examples/offboard_py/offboard_control.py
+++ src/examples/offboard_py/offboard_control.py
@@ -19,10 +19,10 @@ class OffboardControl(Node):
     def __init__(self):
         super().__init__('OffboardControl')
         self.offboard_control_mode_publisher_ = self.create_publisher(OffboardControlMode,
-                                                                        "/fmu/in/offboard_control_mode", 10)
+                                                                        "/fmu/in/offboard_control_mode", 5)
         self.trajectory_setpoint_publisher_ = self.create_publisher(TrajectorySetpoint,
-                                                                    "/fmu/in/trajectory_setpoint", 10)
-        self.vehicle_command_publisher_ = self.create_publisher(VehicleCommand, "/fmu/in/vehicle_command", 10)
+                                                                    "/fmu/in/trajectory_setpoint", 5)
+        self.vehicle_command_publisher_ = self.create_publisher(VehicleCommand, "/fmu/in/vehicle_command", 5)
 
         self.offboard_setpoint_counter_ = 0
 
